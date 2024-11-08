9767476f9cdacba4a29bd630512625b5fcef78bd
xAldox995 <valzanialdo@gmail.com>
2024-11-08 14:18:27 +0100

    finito il filter

--- src/main/java/aldovalzani/progSett_be_m2_w3/security/JWTCheckFilter.java
+++ src/main/java/aldovalzani/progSett_be_m2_w3/security/JWTCheckFilter.java
@@ -1,11 +1,17 @@
 package aldovalzani.progSett_be_m2_w3.security;
 
+import aldovalzani.progSett_be_m2_w3.entities.Utente;
+import aldovalzani.progSett_be_m2_w3.exceptions.UnauthorizedException;
+import aldovalzani.progSett_be_m2_w3.services.UtenteServ;
 import aldovalzani.progSett_be_m2_w3.tools.JWT;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -18,10 +24,21 @@ public class JWTCheckFilter extends OncePerRequestFilter {
     private JWT jwt;
 
     @Autowired
-    private
+    private UtenteServ utenteServ;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-
+        //Sezione di AUTENTICAZINE
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer "))
+            throw new UnauthorizedException("Inserire token nell'Authorization Header nel formato corretto!");
+        String accessToken = authHeader.substring(7);
+        jwt.verifyToken(accessToken);
+        //Sezione di AUTORIZZAZIONE
+        String utenteId = jwt.getIdFromToken(accessToken);
+        Utente currentUtente = this.utenteServ.findUtenteById(Long.parseLong(utenteId));
+        Authentication authentication = new UsernamePasswordAuthenticationToken(currentUtente, null, currentUtente.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        filterChain.doFilter(request, response);
     }
 }
