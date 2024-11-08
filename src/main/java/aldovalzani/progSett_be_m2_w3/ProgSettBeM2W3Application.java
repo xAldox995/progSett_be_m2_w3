f8a57ef084aa1f9803cd7bc8ffd45979a9749021
xAldox995 <valzanialdo@gmail.com>
2024-11-08 11:37:06 +0100

    sett application prop

---
+++ src/main/java/aldovalzani/progSett_be_m2_w3/ProgSettBeM2W3Application.java
@@ -0,0 +1,13 @@
+package aldovalzani.progSett_be_m2_w3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgSettBeM2W3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgSettBeM2W3Application.class, args);
+	}
+
+}
---
+++ src/test/java/aldovalzani/progSett_be_m2_w3/ProgSettBeM2W3ApplicationTests.java
@@ -0,0 +1,13 @@
+package aldovalzani.progSett_be_m2_w3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgSettBeM2W3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
