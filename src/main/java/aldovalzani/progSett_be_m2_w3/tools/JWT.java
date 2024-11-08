78ba72ae4207beed7dadedceec1df67d77d2deb2
xAldox995 <valzanialdo@gmail.com>
2024-11-08 13:22:06 +0100

    finish jwt class

--- src/main/java/aldovalzani/progSett_be_m2_w3/tools/JWT.java
+++ src/main/java/aldovalzani/progSett_be_m2_w3/tools/JWT.java
@@ -1,12 +1,41 @@
 package aldovalzani.progSett_be_m2_w3.tools;
 
+import aldovalzani.progSett_be_m2_w3.entities.Utente;
+import aldovalzani.progSett_be_m2_w3.exceptions.UnauthorizedException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 @Component
 public class JWT {
     @Value("${jwt.secret}")
     private String secret;
 
-    
+    public String createToken(Utente utente) {
+        return Jwts.builder()
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 7))
+                .subject(String.valueOf(utente.getId()))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                .compact();
+    }
+
+    public void verifyToken(String accessToken) {
+        try {
+            Jwts.parser()
+                    .verifyWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                    .build().parse(accessToken);
+        } catch (Exception es) {
+            throw new UnauthorizedException("Problema col token! Rieffetuare il login");
+        }
+
+    }
+
+    public String getIdFromToken(String accessToken) {
+        return Jwts.parser().verifyWith(Keys.hmacShaKeyFor(secret.getBytes())).build().parseSignedClaims(accessToken)
+                .getPayload().getSubject();
+    }
 }
